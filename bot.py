238b07bd0ff1daece3d823151e51b671cdb50d59
Ilya <133566219+LankiSer@users.noreply.github.com>
2025-10-26 18:46:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import telebot
 from handlers import register_handlers
 
-TOKEN = '8258963893:AAFIEwGQRAMA5c5hGd39BJOeHog6rErN-A0'
+TOKEN = ''
 bot = telebot.TeleBot(TOKEN)
 
 register_handlers(bot)
+
 
 bot.polling(none_stop=True)
